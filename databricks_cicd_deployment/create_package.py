391bf628d0bd0faf98cd777cac39bfea841e5013
niruban-manoharan <133218486+niruban-manoharan@users.noreply.github.com>
2023-12-29 10:50:00 +0530

    Update create_package.py

--- databricks_cicd_deployment/create_package.py
+++ databricks_cicd_deployment/create_package.py
@@ -8,7 +8,7 @@ DATABRICKS_API_KEY = os.environ["DATABRICKS_API_KEY"]
 DATABRICKS_URL = os.environ["DEV_DATABRICKS_URL"]
 
 
-f2 = open('/home/runner/work/databricks/components.yml')
+f2 = open('/home/runner/work/databricks/databricks/components.yml')
 
 component_data = load(f2, Loader=Loader)
 
